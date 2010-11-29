2a0fcede0cb09268f65ef3724078b0fea189800a
Joel Clermont <joel@orionweb.net>
2010-11-28 21:17:24 -0600

    remove unneeded CSS comments

--- index.php
+++ index.php
@@ -13,43 +13,38 @@ if ($_POST['submit']) {
 body {
 	font: 100% Verdana, Arial, Helvetica, sans-serif;
 	background: #666666;
-	margin: 0; /* it's good practice to zero the margin and padding of the body element to account for differing browser defaults */
+	margin: 0; 
 	padding: 0;
-	text-align: center; /* this centers the container in IE 5* browsers. The text is then set to the left aligned default in the #container selector */
+	text-align: center;
 	color: #000000;
 }
 
-/* Tips for Elastic layouts 
-1. Since the elastic layouts overall sizing is based on the user's default fonts size, they are more unpredictable. Used correctly, they are also more accessible for those that need larger fonts size since the line length remains proportionate.
-2. Sizing of divs in this layout are based on the 100% font size in the body element. If you decrease the text size overall by using a font-size: 80% on the body element or the #container, remember that the entire layout will downsize proportionately. You may want to increase the widths of the various divs to compensate for this.
-3. If font sizing is changed in differing amounts on each div instead of on the overall design (ie: #sidebar1 is given a 70% font size and #mainContent is given an 85% font size), this will proportionately change each of the divs overall size. You may want to adjust based on your final font sizing.
-*/
 .oneColElsCtrHdr #container {
-	width: 46em;  /* this width will create a container that will fit in an 800px browser window if text is left at browser default font sizes */
+	width: 46em;
 	background: #FFFFFF;
-	margin: 0 auto; /* the auto margins (in conjunction with a width) center the page */
+	margin: 0 auto;
 	border: 1px solid #000000;
-	text-align: left; /* this overrides the text-align: center on the body element. */
+	text-align: left;
 }
 .oneColElsCtrHdr #header { 
 	background: #DDDDDD; 
-	padding: 0 10px 0 20px;  /* this padding matches the left alignment of the elements in the divs that appear beneath it. If an image is used in the #header instead of text, you may want to remove the padding. */
+	padding: 0 10px 0 20px;
 } 
 .oneColElsCtrHdr #header h1 {
-	margin: 0; /* zeroing the margin of the last element in the #header div will avoid margin collapse - an unexplainable space between divs. If the div has a border around it, this is not necessary as that also avoids the margin collapse */
-	padding: 10px 0; /* using padding instead of margin will allow you to keep the element away from the edges of the div */
+	margin: 0;
+	padding: 10px 0;
 }
 .oneColElsCtrHdr #mainContent {
-	padding: 0 20px; /* remember that padding is the space inside the div box and margin is the space outside the div box */
+	padding: 0 20px;
 	background: #FFFFFF;
 }
 .oneColElsCtrHdr #footer { 
-	padding: 0 10px; /* this padding matches the left alignment of the elements in the divs that appear above it. */
+	padding: 0 10px;
 	background:#DDDDDD;
 } 
 .oneColElsCtrHdr #footer p {
-	margin: 0; /* zeroing the margins of the first element in the footer will avoid the possibility of margin collapse - a space between divs */
-	padding: 10px 0; /* padding on this element will create space, just as the the margin would have, without the margin collapse issue */
+	margin: 0;
+	padding: 10px 0;
 }
 -->
 </style></head>
